aaf175b2119bab6ca8ba81c64ff2247cf02f32e0
Jaehwan <dialektike@gmail.com>
2020-02-29 16:39:10 +0900

    Create the_Function_1_4_4.py

---
+++ the_Function_1_4_4.py
@@ -0,0 +1,15 @@
+def boston_cryptograph_encryption(p, k):
+    if p == 0:
+        if k == "♣︎":
+            return 0
+        elif k == "♥︎":
+            return 1
+        else:
+            return 1
+    else:
+        if k == "♣︎":
+            return 0
+        elif k == "♥︎":
+            return 1
+        else:
+            return 1
