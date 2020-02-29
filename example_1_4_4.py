4149b5ce3e2aa63d05f95eac310ec57ddeebdaa3
Hyeonji-Ryu <42489770+Hyeonji-Ryu@users.noreply.github.com>
2020-02-29 17:32:42 +0900

    Create example_1_4_4.py

---
+++ example_1_4_4.py
@@ -0,0 +1,4 @@
+temp = {"A":9,"B":2,"C":2,"D":4,"E":12,"F":2,"G":3, "H":2,"I":9,"J":1,"K":1,"L":1,"M":2,"N":6, "O":8,"P":2,"Q":1,"R":6,"S":4,"T":6,"U":4, "V":2,"W":2,"X":1,"Y":2,"Z":1}
+scrabble = {}
+for i in temp.keys():
+    scrabble.update({i:temp[i]/sum([j for j in temp.values()])})
